38ec342c719f902b8a4e6b2996f7bd312621f949
oxide <mohammedhaider15mo@gmail.com>
2024-09-10 11:15:19 +0300

    REFACTOR: move files and folders listing to a seperate script

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,15 +1,32 @@
 import views from "../views/views.js";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 class IndexController {
   constructor() {}
 
-  indexGet(req, res, next) {
+  async indexGet(req, res, next) {
     if (!req.user) {
       return res.redirect("/login");
     }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: req.user.id,
+      },
+      include: {
+        folders: true,
+        files: true,
+      },
+    });
+    if (!user) {
+      next(new Error("Cannot access user data"));
+    }
+    console.log(user.folders);
     res.render(views.layout, {
       page: views.index,
-      params: {},
+      params: { folders: user.folders, files: user.files },
     });
   }
 }
--- views/views.js
+++ views/views.js
@@ -1,10 +1,18 @@
+import process from "node:process";
+
 class Views {
   constructor() {}
 
-  layout = "layout";
-  index = "index";
-  signup = "signup";
-  login = "login";
+  #rootPath = process.cwd();
+
+  //----------------  Pages
+  layout = this.#rootPath + "/views/layout";
+  index = this.#rootPath + "/views/index";
+  signup = this.#rootPath + "/views/signup";
+  login = this.#rootPath + "/views/login";
+
+  //----------------  Partials
+  fileNodeList = this.#rootPath + "/views/partials/fileNodeList";
 }
 
 const views = new Views();
